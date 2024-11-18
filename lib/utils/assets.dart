14e87d05ef1ff138b4ab24629254835b00682e44
Assil <astradhdbjd@gmail.com>
2024-11-18 20:24:11 +0100

    add fonts & assets

---
+++ lib/utils/assets.dart
@@ -0,0 +1,89 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for iconsCourses
+  /// assets/icons/courses.svg
+  static const String iconsCourses = "assets/icons/courses.svg";
+
+  /// Assets for iconsExcel
+  /// assets/icons/Excel.svg
+  static const String iconsExcel = "assets/icons/Excel.svg";
+
+  /// Assets for iconsHome
+  /// assets/icons/home.svg
+  static const String iconsHome = "assets/icons/home.svg";
+
+  /// Assets for iconsNotification
+  /// assets/icons/notification.svg
+  static const String iconsNotification = "assets/icons/notification.svg";
+
+  /// Assets for iconsOneDrive
+  /// assets/icons/OneDrive.svg
+  static const String iconsOneDrive = "assets/icons/OneDrive.svg";
+
+  /// Assets for iconsOneNote
+  /// assets/icons/OneNote.svg
+  static const String iconsOneNote = "assets/icons/OneNote.svg";
+
+  /// Assets for iconsOutlook
+  /// assets/icons/Outlook.svg
+  static const String iconsOutlook = "assets/icons/Outlook.svg";
+
+  /// Assets for iconsPowerPoint
+  /// assets/icons/PowerPoint.svg
+  static const String iconsPowerPoint = "assets/icons/PowerPoint.svg";
+
+  /// Assets for iconsProfile
+  /// assets/icons/profile.svg
+  static const String iconsProfile = "assets/icons/profile.svg";
+
+  /// Assets for iconsReservations
+  /// assets/icons/reservations.svg
+  static const String iconsReservations = "assets/icons/reservations.svg";
+
+  /// Assets for iconsSharePoint
+  /// assets/icons/SharePoint.svg
+  static const String iconsSharePoint = "assets/icons/SharePoint.svg";
+
+  /// Assets for iconsSkype
+  /// assets/icons/Skype.svg
+  static const String iconsSkype = "assets/icons/Skype.svg";
+
+  /// Assets for iconsTeams
+  /// assets/icons/Teams.svg
+  static const String iconsTeams = "assets/icons/Teams.svg";
+
+  /// Assets for iconsWord
+  /// assets/icons/Word.svg
+  static const String iconsWord = "assets/icons/Word.svg";
+
+  /// Assets for iconsYammer
+  /// assets/icons/Yammer.svg
+  static const String iconsYammer = "assets/icons/Yammer.svg";
+
+  /// Assets for imagesAi
+  /// assets/images/ai.png
+  static const String imagesAi = "assets/images/ai.png";
+
+  /// Assets for imagesHomeBanner
+  /// assets/images/home_banner.png
+  static const String imagesHomeBanner = "assets/images/home_banner.png";
+
+  /// Assets for imagesIntroBackground
+  /// assets/images/intro_background.png
+  static const String imagesIntroBackground = "assets/images/intro_background.png";
+
+  /// Assets for imagesMicrosoft
+  /// assets/images/microsoft.png
+  static const String imagesMicrosoft = "assets/images/microsoft.png";
+
+  /// Assets for imagesPython
+  /// assets/images/python.png
+  static const String imagesPython = "assets/images/python.png";
+
+  /// Assets for imagesSecurity
+  /// assets/images/security.png
+  static const String imagesSecurity = "assets/images/security.png";
+}
+
