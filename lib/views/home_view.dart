eea0bed492dd486c349f696d63e2d3b5ae647212
aconitum001 <skandern0000@gmail.com>
2024-11-18 21:57:21 +0100

    created bottom nav bar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,14 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:quatre_c/utils/app_colors.dart';
+import 'package:quatre_c/utils/assets.dart';
 import 'package:quatre_c/widgets/home_view_body.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int currentIndex = 0;
+
+  final List<Widget> _views = [
+    const HomeViewBody(),
+    const Center(
+      child: Text("Courses View"),
+    ),
+    const Center(
+      child: Text("Reservation View"),
+    ),
+    const Center(
+      child: Text("Profile View"),
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: HomeViewBody(),
+        body: _views[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: currentIndex,
+          onTap: (value) {
+            setState(() {
+              currentIndex = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                Assets.iconsHome,
+                colorFilter: ColorFilter.mode(
+                  AppColors.gray2Color,
+                  BlendMode.srcIn,
+                ),
+              ),
+              activeIcon: SvgPicture.asset(
+                Assets.iconsHome,
+                colorFilter: ColorFilter.mode(
+                  AppColors.primaryColor,
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                Assets.iconsCourses,
+                colorFilter: ColorFilter.mode(
+                  AppColors.gray2Color,
+                  BlendMode.srcIn,
+                ),
+              ),
+              activeIcon: SvgPicture.asset(
+                Assets.iconsCourses,
+                colorFilter: ColorFilter.mode(
+                  AppColors.primaryColor,
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                Assets.iconsReservations,
+                colorFilter: ColorFilter.mode(
+                  AppColors.gray2Color,
+                  BlendMode.srcIn,
+                ),
+              ),
+              activeIcon: SvgPicture.asset(
+                Assets.iconsReservations,
+                colorFilter: ColorFilter.mode(
+                  AppColors.primaryColor,
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                Assets.iconsProfile,
+                colorFilter: ColorFilter.mode(
+                  AppColors.gray2Color,
+                  BlendMode.srcIn,
+                ),
+              ),
+              activeIcon: SvgPicture.asset(
+                Assets.iconsProfile,
+                colorFilter: ColorFilter.mode(
+                  AppColors.primaryColor,
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: "",
+            ),
+          ],
+        ),
       ),
     );
   }
