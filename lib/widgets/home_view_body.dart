c6c279df35fd4e7d603e91d99c069612507abe30
aconitum001 <skandern0000@gmail.com>
2024-11-18 20:21:18 +0100

    created home header section

--- lib/widgets/home_view_body.dart
+++ lib/widgets/home_view_body.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:quatre_c/widgets/home_header_section.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 20,
+      ),
+      child: Column(
+        children: [
+          Gap(20),
+          HomeHeaderSection(
+            title: "Bonjour, Hidaya",
+          ),
+        ],
+      ),
     );
   }
 }
